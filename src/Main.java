4ed53cc850f05e77873da7ad44bd9739db1f576d
Dmitrii Fedotov <fedotovdmz@gmail.com>
2021-08-18 19:27:54 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int Balance = 2_000_000_000;
+        int balance = 2_000_000_000;
         int transfer = 500_000_000;
-        int total = Balance + transfer;
+        int total = balance + transfer;
         System.out.println(total + " " + "рублей");
     }
 }
